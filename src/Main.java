1d2795f08ab36169467848b0d75a727ea3fcca8a
Aleksandra Sovtic <sovtic.aleksandra@gmail.com>
2019-01-24 20:43:20 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World"); 
+        System.out.println("Aleksandra");
+        
+        String user ="Aleksandra Sovtic";
+       
+        Car audi=new Car();
+        
+        System.out.println(user);
+        System.out.println(audi.currentFuel);
+        System.out.println(audi.consumption);
+        System.out.println(audi.model);
+    }
+    
+}
